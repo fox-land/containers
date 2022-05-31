c9e6ce1d5ae27299d2b4aaa49a9f48ba73c1994b
Edwin Kofler <edwin@kofler.dev>
2022-05-30 17:46:46 -0700

    change: Improve build scripts

--- Bakefile.sh
+++ Bakefile.sh
@@ -12,49 +12,67 @@
 # fedora: 35
 
 task.build() {
+	local -a args=()
 	local -a docker_args=()
 
 	for arg do case $arg in
 	--bypass-cache)
-		docker_args+=('--pull' '--no-cache')
+		docker_args+=('--pull' '--no-cache') ;;
+	*)
+		args+=("$arg") ;;
 	esac done
 
 	local commit=
 	commit=$(git rev-parse --short HEAD)
 
-
-	local {pair,distro,distro_variants}=
-	for pair in \
-		'debian|bullseye:buster:stretch' \
-		'ubuntu|jammy:focal:bionic'
-	do
-		distro=${pair%%|*}
-		IFS=':' read -ra distro_variants <<< "${pair#*|}"
-
-		local distro_variant=
-		for distro_variant in "${distro_variants[@]}"; do
-			local date=
-			date=$(date --rfc-3339=seconds)
-
-			bake.info "Building $distro $distro_variant"
-			docker build \
-				--build-arg ARG_DISTRO_VARIANT="$distro_variant" \
-				--build-arg ARG_GIT_COMMIT="$commit" \
-				--file "./$distro/Containerfile" \
-				--label org.opencontainers.image.title="Fox build for $distro_variant" \
-				--label org.opencontainers.image.description="Fox build for $distro_variant" \
-				--label org.opencontainers.image.authors="Edwin Kofler <edwin@kofler.dev" \
-				--label org.opencontainers.image.url="https://github.com/hyperupcall/containers" \
-				--label org.opencontainers.image.documentation="https://github.com/hyperupcall/containers" \
-				--label org.opencontainers.image.source="https://github.com/hyperupcall/containers" \
-				--label org.opencontainers.image.revision="$commit" \
-				--label org.opencontainers.image.created="$date" \
-				--tag "fox.$distro-$distro_variant" \
-				--tag "ghcr.io/hyperupcall/fox.$distro-$distro_variant" \
-				"${docker_args[@]}" \
-				"./$distro"
-
-				docker push "ghcr.io/hyperupcall/fox.$distro-$distro_variant:latest"
+	# Build one or more containers
+	local -A distro_pairs=(
+		[debian]='bullseye:buster:stretch'
+		[ubuntu]='jammy:focal:bionic'
+	)
+	if [ -z "${args[0]}" ]; then
+		local key= value=
+		for key in "${!distro_pairs[@]}"; do
+			local value="${distro_pairs[$key]}"
+			util.build_container "$key|$value"
 		done
+	else
+		local key='debian'
+		local value="${distro_pairs[$key]}"
+		util.build_container "$key|$value"
+	fi
+}
+
+
+util.build_container() {
+	local pair="$1"
+
+	distro=${pair%%|*}
+	IFS=':' read -ra distro_variants <<< "${pair#*|}"
+
+	local distro_variant=
+	for distro_variant in "${distro_variants[@]}"; do
+		local date=
+		date=$(date --rfc-3339=seconds)
+
+		bake.info "Building $distro $distro_variant"
+		docker build \
+			--build-arg ARG_DISTRO_VARIANT="$distro_variant" \
+			--build-arg ARG_GIT_COMMIT="$commit" \
+			--file "./$distro/Containerfile" \
+			--label org.opencontainers.image.title="Fox build for $distro_variant" \
+			--label org.opencontainers.image.description="Fox build for $distro_variant" \
+			--label org.opencontainers.image.authors="Edwin Kofler <edwin@kofler.dev" \
+			--label org.opencontainers.image.url="https://github.com/hyperupcall/containers" \
+			--label org.opencontainers.image.documentation="https://github.com/hyperupcall/containers" \
+			--label org.opencontainers.image.source="https://github.com/hyperupcall/containers" \
+			--label org.opencontainers.image.revision="$commit" \
+			--label org.opencontainers.image.created="$date" \
+			--tag "fox.$distro-$distro_variant" \
+			--tag "ghcr.io/hyperupcall/fox.$distro" \
+			"${docker_args[@]}" \
+			"./$distro"
+
+			docker push "ghcr.io/hyperupcall/fox.$distro:latest"
 	done
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Containers
 
-My containers
+Sometimes, it's tedius to 
